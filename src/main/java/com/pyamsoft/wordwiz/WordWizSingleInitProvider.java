bb5bcd3fbaff4bcf84073d1e9ec28219c9550f09
pyamsoft <pyam.soft@gmail.com>
2016-12-13 18:16:43 -0800

    Format

--- src/main/java/com/pyamsoft/wordwiz/WordWizSingleInitProvider.java
+++ src/main/java/com/pyamsoft/wordwiz/WordWizSingleInitProvider.java
@@ -17,7 +17,6 @@
 package com.pyamsoft.wordwiz;
 
 import android.content.Context;
-import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import com.pyamsoft.pydroid.IPYDroidApp;
