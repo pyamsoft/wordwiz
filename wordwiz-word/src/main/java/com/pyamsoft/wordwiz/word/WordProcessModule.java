91f21e4bdd3829de4391f3e9ad26717b8ec0a27d
pyamsoft <pyam.soft@gmail.com>
2017-03-08 19:16:16 -0800

    Update pydroid, rxjava2

--- wordwiz-word/src/main/java/com/pyamsoft/wordwiz/word/WordProcessModule.java
+++ wordwiz-word/src/main/java/com/pyamsoft/wordwiz/word/WordProcessModule.java
@@ -19,7 +19,7 @@ package com.pyamsoft.wordwiz.word;
 import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
 import com.pyamsoft.wordwiz.base.WordWizModule;
-import rx.Scheduler;
+import io.reactivex.Scheduler;
 
 public class WordProcessModule {
 
