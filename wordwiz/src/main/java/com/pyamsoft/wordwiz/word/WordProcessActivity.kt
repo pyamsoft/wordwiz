e6f64337608f8736e6c634f8e0cab79517dada84
pyamsoft <pyam.soft@gmail.com>
2017-10-21 11:46:57 -0700

    Update pydroid

--- wordwiz/src/main/java/com/pyamsoft/wordwiz/word/WordProcessActivity.kt
+++ wordwiz/src/main/java/com/pyamsoft/wordwiz/word/WordProcessActivity.kt
@@ -25,22 +25,22 @@ import com.pyamsoft.pydroid.ui.app.activity.DisposableActivity
 import com.pyamsoft.pydroid.ui.helper.Toasty
 import com.pyamsoft.wordwiz.Injector
 import com.pyamsoft.wordwiz.WordWizComponent
+import com.pyamsoft.wordwiz.word.WordProcessPresenter.View
 import timber.log.Timber
 
-abstract class WordProcessActivity : DisposableActivity() {
+abstract class WordProcessActivity : DisposableActivity(), View {
 
   internal lateinit var presenter: WordProcessPresenter
 
   override fun provideBoundPresenters(): List<Presenter<*>> = listOf(presenter)
 
-  override val shouldConfirmBackPress: Boolean
-    get() = false
+  override val shouldConfirmBackPress: Boolean = false
 
   override fun onCreate(savedInstanceState: Bundle?) {
     overridePendingTransition(0, 0)
     super.onCreate(savedInstanceState)
     Injector.obtain<WordWizComponent>(applicationContext).inject(this)
-    presenter.bind(Unit)
+    presenter.bind(this)
     handleIntent(intent)
   }
 
@@ -57,26 +57,34 @@ abstract class WordProcessActivity : DisposableActivity() {
   private fun handleIntent(intent: Intent) {
     Timber.d("Handle a process text intent")
     val text = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)
-    presenter.handleActivityLaunchType(componentName, text, getIntent().extras, onProcessBegin = {
-      Timber.d("Start processing")
-    }
-        , onProcessError = {
-      Timber.e(it, "An error occurred while attempting to process text")
-      Toasty.makeText(applicationContext,
-          "An error occurred while attempting to process text", Toasty.LENGTH_SHORT).show()
-    }, onProcessTypeWordCount = {
-      Toasty.makeText(applicationContext, "Word count: $it", Toasty.LENGTH_SHORT)
-          .show()
-    }, onProcessTypeLetterCount = {
-      Toasty.makeText(applicationContext, "Letter count: $it",
-          Toasty.LENGTH_SHORT).show()
+    presenter.handleActivityLaunchType(componentName, text, getIntent().extras)
+  }
 
-    }, onProcessTypeOccurrences = { occurrences, snippet ->
-      Toasty.makeText(applicationContext, "Occurrence count of snippet '$snippet': $occurrences",
-          Toasty.LENGTH_SHORT).show()
-    }, onProcessComplete = {
-      Timber.d("Process complete")
-      finish()
-    })
+  override fun onProcessBegin() {
+    Timber.d("Start processing")
+  }
+
+  override fun onProcessComplete() {
+    Timber.d("Process complete")
+    finish()
+  }
+
+  override fun onProcessError(throwable: Throwable) {
+    Timber.e(throwable, "An error occurred while attempting to process text")
+    Toasty.makeText(applicationContext,
+        "An error occurred while attempting to process text", Toasty.LENGTH_SHORT).show()
+  }
+
+  override fun onProcessTypeWordCount(count: Int) {
+    Toasty.makeText(applicationContext, "Word count: $count", Toasty.LENGTH_SHORT).show()
+  }
+
+  override fun onProcessTypeLetterCount(count: Int) {
+    Toasty.makeText(applicationContext, "Letter count: $count   ", Toasty.LENGTH_SHORT).show()
+  }
+
+  override fun onProcessTypeOccurrences(count: Int, snippet: String) {
+    Toasty.makeText(applicationContext, "Occurrence count of snippet '$snippet': $count",
+        Toasty.LENGTH_SHORT).show()
   }
 }
